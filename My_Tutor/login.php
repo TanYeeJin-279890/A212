433c0e6754263bde8f66043937a7332c8dd1de9f
TanYeeJin-279890 <tan_yee_jin@soc.uum.edu.my>
2022-05-24 16:16:39 +0800

    new

--- My_Tutor/login.php
+++ My_Tutor/login.php
@@ -1,3 +1,29 @@
+<?php
+
+if (isset($_POST['signin'])) {
+    include 'dbconnect.php';
+    $email = $_POST['email'];
+    $pass = sha1($_POST['password']);
+    $sqllogin = "SELECT * FROM tbl_register WHERE user_email = '$email' AND user_password = '$pass'";
+    $stmt = $conn->prepare($sqllogin);
+    $stmt->execute();
+    $number_of_rows = $stmt->fetchColumn();
+    
+    if ($number_of_rows  > 0) {
+        session_start();
+        $_SESSION["sessionid"] = session_id();
+        $_SESSION["email"] = $email ;
+        echo "<script>alert('Login Success');</script>";
+        echo "<script> window.location.replace('dashboard.php')</script>";
+    } else {
+        echo "<script>alert('Login Failed');</script>";
+        echo "<script> window.location.replace('login.php')</script>";
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,7 +45,9 @@
 
 </head>
 
-<body>
+
+<!--cookies = Without cookies, you’d have to login again after you leave a site or rebuild your shopping cart if you accidentally close the page. -->
+<body onload="loadCookies()">
     <header class="w3-header w3-red w3-center w3-padding-24">
         <h2><b>My Tutor Websites</b></h2>
     </header>
@@ -58,7 +86,7 @@
                 <label>Remember Me</label>
             </p>
             <p>
-                <input type="button" class="w3-btn w3-red" value="Sign Up">
+                <input type="button" class="w3-btn w3-red" name="signin" value="Sign Up">
             </p>
 
             <div><a href="php/register1.php">Not yet registered? Click Me!</a></div>
--- My_Tutor/php/register1.php
+++ My_Tutor/php/register1.php
@@ -1,16 +1,17 @@
 <?php
+
 if (isset($_POST['register'])) {
     include_once("dbconnect.php");
 
     if (!(isset($_POST["name"]) || isset($_POST["email"]) || isset($_POST["phone"]) || isset($_POST["password"]) || isset($_POST["address"]))) {
-        echo "<script> alert('Please fill in all required information')</script>";
+        echo "<script> alert('Please fill in all informations')</script>";
         echo "<script> window.location.replace('register1.php')</script>";
     } else {
         if (file_exists($_FILES["fileToUpload"]["tmp_name"]) || is_uploaded_file($_FILES["fileToUpload"]["tmp_name"])) {
 
             $name = $_POST["name"];
             $address = $_POST["address"];
-            $password = $_POST["password"];
+            $password = sha1($_POST["password"]);
             $email = $_POST["email"];
             $phone = $_POST["phone"];
 
@@ -21,7 +22,7 @@ if (isset($_POST['register'])) {
                 $conn->exec($sqlregister);
                 uploadImage($icno);
                 echo "<script>alert('Registration successful')</script>";
-                echo "<script>window.location.replace('../login.html')</script>";
+                echo "<script>window.location.replace('../login.php')</script>";
             } catch (PDOException $e) {
                 echo "<script>alert('Registration failed')</script>";
                 echo "<script>window.location.replace('register1.php')</script>";
@@ -30,14 +31,14 @@ if (isset($_POST['register'])) {
 
             $name = $_POST["name"];
             $address = $_POST["address"];
-            $password = $_POST["password"];
+            $password = sha1($_POST["password"]);
             $email = $_POST["email"];
             $phone = $_POST["phone"];
             $sqlregister = "INSERT INTO `tbl_register`( `name`, `address`, `password`, `email`, `phone`) VALUES( '$name', '$address', '$password', '$email', '$phone')";
             try {
                 $conn->exec($sqlregister);
                 echo "<script>alert('Registration successful')</script>";
-                echo "<script>window.location.replace('../login.html')</script>";
+                echo "<script>window.location.replace('../login.php')</script>";
             } catch (PDOException $e) {
                 echo "<script>alert('Registration failed')</script>";
                 echo "<script>window.location.replace('register1.php')</script>";
@@ -67,6 +68,7 @@ function uploadImage($email)
     <link rel="stylesheet" href="../css/w3.css">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <title>Registration Page</title>
 
 
@@ -76,28 +78,64 @@ function uploadImage($email)
 
         }
 
+        /* provide slide show background */
+        #SlideShow_bg {
+            width: 100%;
+            height: 100vh;
+            opacity: 0.8;
+            background-position: bottom center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            backface-visibility: hidden;
+            animation: slideShow 8s linear infinite 0s;
+            animation-timing-function: ease-in-out;
+            background-image: url('../res/image/tutor_course/Slide1.png');
+
+        }
+
+        @keyframes slideShow {
+            0% {
+                background-image: url('../res/image/tutor_course/Slide1.png');
+            }
+
+            20% {
+                background-image: url('../res/image/tutor_course/Slide2.png');
+            }
+
+            40% {
+                background-image: url('../res/image/tutor_course/Slide3.png');
+            }
+
+            60% {
+                background-image: url('../res/image/tutor_course/Slide4.png');
+            }
+
+            80% {
+                background-image: url('../res/image/tutor_course/Slide5.png');
+            }
+
+            100% {
+                background-image: url('../res/image/tutor_course/Slide6.png');
+            }
+        }
+
         .header {
             overflow: hidden;
             background-color: red;
             padding: 20px 10px;
         }
 
-        /* Create two unequal columns that floats next to each other */
+        .label {
+            color: black;
+        }
+
         .column {
-            float: left;
+
+
+            float: center;
             padding: 5px;
             height: 300px;
-            
 
-        }
-
-        .left {
-            width: 50%;
-            
-        }
-
-        .right {
-            width: 50%;
 
         }
 
@@ -106,9 +144,11 @@ function uploadImage($email)
             content: "";
             display: table;
             clear: both;
-            
+
+
         }
     </style>
+
 </head>
 
 <body>
@@ -118,68 +158,89 @@ function uploadImage($email)
         </div>
     </div>
 
-    <div class="w3-content">
-        <div class="column left">
-            <img src="../Capture.PNG" style="width:400px; height:auto;"><br>
-        </div>
+    <div id="SlideShow_bg">
 
-        <div class="column right w3-padding-30" style="display:flex; justify-content: center">
-            <form action="register1.php" method="post" enctype="multipart/form-data" onsubmit="return confirmDialog()">
-                <div class="w3-container w3-card w3-padding w3-margin" style="width:600px;margin:auto;text-align:left;">
+        <div class="w3-content">
+            <div class="column w3-padding-30" style="display:flex; justify-content: center">
+                <form action="register1.php" method="post" enctype="multipart/form-data" onsubmit=" return confirmDialog()">
+                    <div class="w3-container w3-card w3-padding w3-margin" style="width:600px;margin:auto;text-align:left;background-color: lavenderBlush;">
 
-                    <h2 style="text-align:center;font-weight:bold;">Registration Form</h2>
+                        <h2 style="text-align:center;font-weight:bold;">Registration Form</h2>
 
-                    <div class="w3-container w3-center">
-                        <img class="imgselection" src="../res/image/userpic.png" style="width:30%"><br>
-                        <div>
-                            <h4>Upload your Profile Image</h4><br>
+                        <div class="w3-container w3-center ">
+                            <img class="imgselection" src="../res/image/userpic.png" style="width:30%"><br>
+                            <div>
+                                <h4>Upload your Profile Image</h4><br>
+                            </div>
+                            <input type="file" onchange="previewFile()" name="fileToUpload" id="fileToUpload"><br>
                         </div>
-                        <input type="file" onchange="previewFile()" name="fileToUpload" id="fileToUpload"><br>
-                    </div>
 
-                    <div>
-                        <p>
-                            <label><b>Name</b></label>
-                            <input class="w3-input w3-round w3-border" type="text" name="name" placeholder="Your Name" required>
-                        </p>
+                        <div class="label">
+                            <p>
+                                <label><b>Name</b></label>
+                                <input class="w3-input w3-round w3-border" type="text" name="name" placeholder="Your Name" required>
 
-                        <p>
-                            <label><b>Email</b></label>
-                            <input class="w3-input w3-round w3-border" type="text" name="email" placeholder="Your Email Address" required>
-                        </p>
-
-                        <p>
-                            <label><b>Password</b></label>
-                            <input class="w3-input w3-round w3-border" type="text" name="password" placeholder="Your Password" required>
-                        </p>
-
-                        <p>
-                            <label><b>Re-enter Password</b></label>
-                            <input class="w3-input w3-round w3-border" type="text" name="repassword" placeholder="Your Password" required>
-                        </p>
-
-                        <p>
-                            <label><b>Phone</b></label>
-                            <input class="w3-input w3-round w3-border" type="text" name="phone" placeholder="Your Phone No." required>
-                        </p>
-
-                        <p>
-                            <label><b>Address</b></label>
-                            <input class="w3-input w3-round w3-border" type="text" name="address" placeholder="Your Home Address" required>
-                        </p>
-                        <br>
-                        <div class="row">
-                            <p style="text-align: center;">
-                                <input class="w3-btn w3-red w3-round w3-padding-20" type="submit" name="register" value="Register">
                             </p>
-                        </div>
-                    </div>
 
-                </div>
-            </form>
+                            <p>
+                                <label><b>Email</b></label>
+                                <input class="w3-input w3-round w3-border" type="text" name="email" placeholder="Your Email Address" required>
+                            </p>
+
+                            <p>
+                                <label><b>Password</b></label>
+                            <div class="input-icon">
+
+                                <input class="w3-input w3-round w3-border" type="password" name="password" id="Mypassword" placeholder="Your Password" id="password" required>
+                                <input class="w3-btn w3-round w3-border" type="checkbox" name="password" id="password" value="pass" onclick="showPass()" required>
+                                <label for="password">Show Password</label>
+
+                            </div>
+                            </p>
+                           
+
+                            <p>
+                                <label><b>Re-enter Password</b></label>
+                            <div class="input-icon">
+
+                                <input class="w3-input w3-round w3-border" type="password" name="repassword" id="Mypassword" placeholder="Enter Again your Password" id="password" required>
+
+                            </div>
+                            </p>
+                            <script>
+                                function showPass() {
+                                    var x = document.getElementById("Mypassword");
+                                    if (x.type === "password") {
+                                        x.type = "text";
+                                    } else {
+                                        x.type = "password";
+                                    }
+                                }
+                            </script>
+
+
+                            <p>
+                                <label><b>Phone</b></label>
+                                <input class="w3-input w3-round w3-border" type="text" name="phone" placeholder="Your Phone No." required>
+                            </p>
+
+                            <p>
+                                <label><b>Address</b></label>
+                                <input class="w3-input w3-round w3-border" type="text" name="address" placeholder="Your Home Address" required>
+                            </p>
+                            <br>
+                            <div class="row">
+                                <p style="text-align: center;">
+                                    <input class="w3-btn w3-red w3-round w3-padding-20" type="submit" name="register" value="Register">
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-
 
 </body>
 
